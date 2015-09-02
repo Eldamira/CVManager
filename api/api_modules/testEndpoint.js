eacc78dce2afc5a1e80c3bc7026feeee21319d56
Lakshin Karunaratne <lakshink@99x.lk>
2015-09-02 10:15:34 +0530

    Before Jasmine

--- api/api_modules/testEndpoint.js
+++ api/api_modules/testEndpoint.js
@@ -1,8 +1,15 @@
 (function () {
 	'use strict';
 	
-	module.exports = function (req, res) {
+	/*module.exports = function (req, res) {
 		res.status(200).json({ message: 'Hi Machan :)' });
-	};
-	
+	};*/
+	module.exports = function() {
+		
+		return {
+			test: function(req,callback){
+				callback('Hi Machan :)');
+			}		
+		};		
+	};	
 })();
