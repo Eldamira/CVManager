909aeadcd8774128941b4fc8615babddf8019e1c
Kasun Kodagoda <kvkrusl@gmail.com>
2015-10-06 15:14:50 +0530

    Add powershell script to run bower install

---
+++ bower_install.ps1
@@ -0,0 +1,7 @@
+Write-Verbose "*********************************"
+Write-Verbose "*  Installing Bower Components  *"
+Write-Verbose "*********************************"
+	
+bower install
+
+Write-Verbose "******** End of Script **********"
